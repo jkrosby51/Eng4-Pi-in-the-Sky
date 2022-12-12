c7bac121544a0043f0d0168d973d2e0740fcc543
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2022-12-12 14:08:04 -0500

    Update PiSkyStarter.py

--- code/PiSkyStarter.py
+++ code/PiSkyStarter.py
@@ -1,4 +1,6 @@
-#https://learn.adafruit.com/using-mpl3115a2-with-circuitpython
+# https://learn.adafruit.com/using-mpl3115a2-with-circuitpython
+# https://www.tomshardware.com/how-to/get-wi-fi-internet-on-raspberry-pi-pico
+# https://www.howtoforge.com/using-an-android-smartphone-as-a-wlan-hotspot
 
 # type: ignore
 import board
@@ -9,15 +11,15 @@ import pwmio
 import busio
 import adafruit_mpl3115a2
 
-sda_pin = board.GP16        #UPDATE AS NEEDED
-scl_pin = board.GP17        #UPDATE AS NEEDED
+sda_pin = board.GP14        #UPDATE AS NEEDED
+scl_pin = board.GP15        #UPDATE AS NEEDED
 i2c = busio.I2C(scl_pin, sda_pin)
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 
 
-sealevel_kPa = 102.29                           ### Find current sea level kPa in Charlottesville here: https://barometricpressure.app/results?lat=38.0386569&lng=-78.4846401
-sensor.sealevel_pressure = sealevel_kPa * 1000  ### Manually set sealevel pressure (in Pascals) based on current weather data for more accuracy
+sealevel_Pa = 102290                           ### Find current sea level kPa in Charlottesville here: https://barometricpressure.app/results?lat=38.0386569&lng=-78.4846401
+sensor.sealevel_pressure = sealevel_Pa         ### Manually set sealevel pressure (in Pascals) based on current weather data for more accuracy
 
 while True:
 
-    print('Altitude: {0:0.3f} meters'.format(sensor.altitude))
+    print(f"Altitude: {sensor.altitude} meters")
