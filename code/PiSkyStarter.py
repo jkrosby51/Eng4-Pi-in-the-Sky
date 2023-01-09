2b35505de3d613304cbb06ac8e42739617bbcc55
jmuss07 <jmuss07@charlottesvilleschools.org>
2023-01-09 14:38:16 -0500

    more message stuff

--- code/PiSkyStarter.py
+++ code/PiSkyStarter.py
@@ -20,6 +20,7 @@ sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
 sealevel_Pa = 102290                           ### Find current sea level kPa in Charlottesville here: https://barometricpressure.app/results?lat=38.0386569&lng=-78.4846401
 sensor.sealevel_pressure = sealevel_Pa         ### Manually set sealevel pressure (in Pascals) based on current weather data for more accuracy
 altitude_initial = sensor.altitude #sets initial altitude to be starting altitude instead of sea-level
+max_altitude = 22 #temporary value
 
 msg1 = ["helloo!", "n"]
 msg2 = ["put me down!", "n"]
@@ -37,3 +38,45 @@ msg11 = ["aaaaaaaaa", "n"]
 while True:
 
     print(f"Altitude: {sensor.altitude} meters")
+    
+    if (sensor.altitude - altitude_initial) > 0 and (sensor.altitude - altitude_initial) < 2 and message1[1] == "n": #temporary altitude values
+        print message1[0]
+        message1[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 2 and (sensor.altitude - altitude_initial) < 4 and message2[1] == "n": #temporary altitude values
+        print message2[0]
+        message2[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 4 and (sensor.altitude - altitude_initial) < 6 and message3[1] == "n": #temporary altitude values
+        print message3[0]
+        message3[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 6 and (sensor.altitude - altitude_initial) < 8 and message4[1] == "n": #temporary altitude values
+        print message4[0]
+        message4[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 8 and (sensor.altitude - altitude_initial) < 10 and message5[1] == "n": #temporary altitude values
+        print message5[0]
+        message5[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 10 and (sensor.altitude - altitude_initial) < 12 and message6[1] == "n": #temporary altitude values
+        print message6[0]
+        message6[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 12 and (sensor.altitude - altitude_initial) < 14 and message7[1] == "n": #temporary altitude values
+        print message7[0]
+        message7[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 14 and (sensor.altitude - altitude_initial) < 16 and message8[1] == "n": #temporary altitude values
+        print message8[0]
+        message8
+        [1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 16 and (sensor.altitude - altitude_initial) < 18 and message9[1] == "n": #temporary altitude values
+        print message9[0]
+        message9[1] = "y"
+        
+    if (sensor.altitude - altitude_initial) > 18 and (sensor.altitude - altitude_initial) < 20 and message10[1] == "n": #temporary altitude values
+        print message10[0]
+        message10[1] = "y"
+    
