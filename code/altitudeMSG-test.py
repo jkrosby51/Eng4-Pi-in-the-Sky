9c60d6a59f8365d3b94790c4979037043edcb509
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2023-01-13 14:11:28 -0500

    Create altitudeMSG-test.py

---
+++ code/altitudeMSG-test.py
@@ -0,0 +1,11 @@
+"""
+- Find library for sound card on circuit python
+- replace all play(#) with correct command and trigger
+"""
+
+
+
+while True:
+    current_altitude = 0 # Will be set to correct altitude in real code
+    
+
